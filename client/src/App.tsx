b35da2b543f8bddc1d9d8237b49e3812676a6d23
Huy Jack <huy.anh.nguyen178@gmail.com>
2018-10-09 08:36:18 +0700

    Init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
+import Grid from "@material-ui/core/Grid";
+
 import * as React from "react";
 import "./App.css";
-
-import logo from "./logo.svg";
+import TopBar from "./components/TopBar";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload. 11
-        </p>
-      </div>
+      <Grid container={true} spacing={16}>
+        <TopBar />
+        <Grid item={true} xs={2}>
+          SideBav
+        </Grid>
+        <Grid item={true} xs={10}>
+          Main
+        </Grid>
+      </Grid>
     );
   }
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { EnthusiasmAction } from "./actions";
-import Hello from "./containers/Hello";
-import { enthusiasm } from "./reducers/index";
-import { StoreState } from "./types/index";
+import App from "./App";
 
-const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
-  enthusiasmLevel: 1,
-  languageName: "TypeScript"
-});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Hello />
-  </Provider>,
-  document.getElementById("root") as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
